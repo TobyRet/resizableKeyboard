f28ce618c34d76e0dd1d640d0aa5ab025c0b8186
Toby Retallick <toby.retallick@gmail.com>
2014-10-14 16:42:33 +0100

    Created keyboard as separate popup window

--- src/sample/Keyboard/ResizableKeyboardSample.java
+++ src/sample/Keyboard/ResizableKeyboardSample.java
@@ -1,20 +1,51 @@
 package sample.Keyboard;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ResizableKeyboardSample extends Application {
+    private boolean keyboardNotShown;
+
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     public void start(final Stage stage) throws Exception {
+        keyboardNotShown = true; //does it make a difference?
 
+        VBox rootBox = new VBox(5);
+        rootBox.setPadding(new Insets(10));
 
-        Keyboard keyboard = new Keyboard(stage);
-        Scene scene = new Scene(keyboard);
-        style(scene);
+        TextField textField1 = new TextField();
+        TextField textField2 = new TextField();
+        Keyboard popupKeyboard = new Keyboard(stage);
+
+        Stage keyboardPopupWindow = new Stage();
+        keyboardPopupWindow.initModality(Modality.NONE);
+        keyboardPopupWindow.initOwner(stage);
+        Scene keyboardScene = new Scene(popupKeyboard);
+        style(keyboardScene);
+        keyboardPopupWindow.setScene(keyboardScene);
+
+        Button showKeyboardButton = new Button("Popup Keyboard");
+        showKeyboardButton.setOnAction(event -> {
+            if(keyboardNotShown) {
+                keyboardPopupWindow.show();
+            } else {
+                keyboardPopupWindow.close();
+            }
+            keyboardNotShown = !keyboardNotShown;
+        });
+
+        rootBox.getChildren().addAll(textField1, textField2, showKeyboardButton);
+
+        Scene scene = new Scene(rootBox);
         displayScene(stage, scene);
     }
 
